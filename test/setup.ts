5305b8d10d51873a423cc1e7328551f315377a4c
rizkymalm <rizkymalm@gmail.com>
2024-08-19 23:35:04 +0700

    vitest

---
+++ test/setup.ts
@@ -0,0 +1,12 @@
+import 'vitest-dom/extend-expect';
+
+// import { cleanup } from '@testing-library/react';
+import { afterEach, vi } from 'vitest';
+
+vi.mock('~/lib/auth', () => {
+    const validateRequest = () => ({ session: null, user: null });
+    return { validateRequest: { validateRequest } };
+});
+
+// Hooks are reset before each suite
+afterEach(() => {});
