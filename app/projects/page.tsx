841d0e65abac37e75648322ee7b13ca5750b5e1e
rizkymalm <rizkymalm@gmail.com>
2024-08-26 09:25:45 +0700

    styling layout

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
+import React from 'react';
 import ButtonToggle from '@/components/button/ButtonToggle';
 import { useTheme } from 'next-themes';
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { motion, Variants } from 'framer-motion';
 
 const menuBar = [
@@ -17,7 +18,7 @@ const menuBar = [
     },
 ];
 
-export default function Home() {
+const ProjectPage = () => {
     const { setTheme } = useTheme();
     const [isChecked, setIsChecked] = useState(true);
 
@@ -64,25 +65,11 @@ export default function Home() {
                 variants={pageTransition}
             >
                 <main className="flex min-h-screen flex-col items-center justify-between p-5 lg:p-20">
-                    <div className="relative z-10 w-full max-w-5xl items-center justify-between py-15 font-mono text-sm lg:flex lg:py-0">
-                        {/* <div className="triangle absolute left-0 right-0 top-0 m-auto w-[50%] bg-black dark:bg-blue-900">
-                    <div className="absolute"></div>
-                </div> */}
-                        <div className="relative left-0 top-0 m-auto flex h-75 w-[80%] rounded-lg border-4 border-black bg-black dark:border-black dark:bg-transparent lg:h-180 lg:w-[50%]">
-                            <div className="profile-photo absolute bottom-[-20px] right-[-20px] h-full w-full rounded-lg border-4 border-black bg-white dark:border-black dark:bg-black"></div>
-                        </div>
-                        <div className="w-full px-10 py-10 lg:w-[50%] lg:py-1">
-                            <div className="text-title-md font-bold lg:text-title-xl">
-                                Hello, I am <br />
-                                <b className="text-yellow-600">RIZKI MALEM</b>
-                            </div>
-                            <div className="text-text-xxl">
-                                Frontend Developer
-                            </div>
-                        </div>
-                    </div>
+                    Project
                 </main>
             </motion.div>
         </div>
     );
-}
+};
+
+export default ProjectPage;
