0a74cdde3cdefafe87d3012e683696011e853546
rizkymalm <rizkymalm@gmail.com>
2026-02-20 04:40:21 +0700

    fix: base url

--- components/ui/Menu.tsx
+++ components/ui/Menu.tsx
@@ -16,7 +16,9 @@ const Menu = () => {
         <div className="fixed inset-x-0 bottom-0 z-999 m-auto h-20 w-full p-15 md:w-1/2">
             <div className="ty-body relative z-9 flex w-full justify-between font-semibold text-accent-dark-active [&>div]:cursor-pointer [&>div]:transition-all [&>div]:duration-300">
                 <div className="hover:text-accent-dark">About Me</div>
-                <div className="hover:text-accent-dark">Project</div>
+                <div className="hover:text-accent-dark">
+                    Project{process.env.API_BASE_URL}
+                </div>
                 <div className="hover:text-accent-dark">Contact</div>
             </div>
             <motion.div
