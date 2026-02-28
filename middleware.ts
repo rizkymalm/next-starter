e16b513899e7b9c738d55031e16cebb6e40e93c5
rizkymalm <rizkymalm@gmail.com>
2026-03-01 02:16:56 +0700

    fix: allow cors origin url

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,7 @@ const allowedOrigins = [
     'https://www.rizkymalm.space',
     'https://rizkymalm.site',
     'https://www.rizkymalm.site',
+    'https://next-gmbh-test.vercel.app/',
 ];
 
 export function middleware(request: NextRequest) {
