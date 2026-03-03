9c48e03dddecba51863831a6db6a1239f6f72458
rizkymalm <rizkymalm@gmail.com>
2026-03-03 19:41:42 +0700

    fix: signup role

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -2,9 +2,7 @@ import bcryptjs from 'bcryptjs';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-import { Messages } from '@/app/constants/messagesEnum';
 import connectMongoDB from '@/lib/db';
-import Role from '@/lib/models/roleModels';
 import User from '@/lib/models/userModels';
 
 export async function POST(request: NextRequest) {
@@ -12,24 +10,13 @@ export async function POST(request: NextRequest) {
         await connectMongoDB();
         const { username, email, password, role } = await request.json();
         const user = await User.findOne({ email });
-        const roles = await Role.findOne({ _id: role });
         if (user) {
             return NextResponse.json(
                 {
                     statusCode: 1409,
-                    message: Messages.SIGNUP_USER_EXIST,
+                    message: 'User already exist',
                 },
                 { status: 409 }
-            );
-        }
-        // check role
-        if (!roles) {
-            return NextResponse.json(
-                {
-                    statusCode: 1404,
-                    message: Messages.SIGNUP_ROLE_NOT_FOUND,
-                },
-                { status: 404 }
             );
         }
         const salt = await bcryptjs.genSalt(10);
@@ -38,11 +25,11 @@ export async function POST(request: NextRequest) {
             username,
             email,
             password: hashedPassword,
-            role,
+            role
         });
         const savedUser = await newUser.save();
         return NextResponse.json({
-            message: Messages.SIGNUP_POST_SUCCESS,
+            message: 'User created successfully',
             savedUser,
         });
     } catch (error: any) {
